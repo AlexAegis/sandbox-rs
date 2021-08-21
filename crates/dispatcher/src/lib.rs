a2d0645f3c393eacb728a4dd3a9b3c7333d75590
AlexAegis <alexaegis@gmail.com>
2021-08-21 10:59:27 +0200

    added unsubscribe mechanic

--- crates/dispatcher/src/lib.rs
+++ crates/dispatcher/src/lib.rs
@@ -1,57 +1,106 @@
-use std::fmt::Debug;
+use std::{
+	collections::HashMap,
+	fmt::Debug,
+	marker::PhantomData,
+	sync::{Arc, Mutex},
+};
 
 pub fn hello_dispatcher() {
 	println!("Hello Dispatcher!");
 }
 
-pub struct Subject<T: Send + Debug> {
-	subscribers: Vec<AnyObserver<T>>,
+pub struct Subject<'subject, T: Send + Debug + Sized> {
+	index: Arc<Mutex<usize>>,
+	subscribers: Arc<Mutex<HashMap<usize, AnyObserver<T>>>>,
+	phantom: PhantomData<&'subject T>,
 }
 
-impl<T> Subject<T>
+impl<'subject, T> Subject<'subject, T>
 where
-	T: Send + Debug,
+	T: Send + Sized + Debug,
 {
 	pub fn new() -> Self {
+		log::debug!("Created Subject!");
 		Self {
-			subscribers: Vec::new(),
+			phantom: PhantomData {},
+			index: Arc::new(Mutex::new(0)),
+			subscribers: Arc::new(Mutex::new(HashMap::new())),
 		}
+	}
+
+	fn unsubscribe(&self, index: usize) {
+		log::debug!("Unsubscribe index: {}", index);
+		self.subscribers.lock().unwrap().remove_entry(&index);
 	}
 }
 
-pub trait Observable<T: Send + Debug> {
+pub trait Observable<'observable, T: Send + Sized + Debug> {
 	fn next(&self, t: T);
 
-	fn subscribe(&mut self, observer: AnyObserver<T>);
+	fn subscribe<'subscription>(
+		&'observable self,
+		observer: AnyObserver<T>,
+	) -> Subscription<'observable, T>;
 }
 
-pub enum AnyObserver<T: Send + Debug> {
+pub enum AnyObserver<T: Send + Sized + Debug> {
 	Observer(Box<dyn Observer<T>>),
 	ClojureObserver(Box<dyn Fn(&T) -> ()>),
 }
 
-impl<T: Send + Debug> Observable<T> for Subject<T> {
+impl<'observable, T: Send + Sized + Debug> Observable<'observable, T> for Subject<'observable, T> {
 	fn next(&self, t: T) {
-		for subscriber in &self.subscribers {
+		log::debug!("Notifying subscribers about {:?}", t);
+		for subscriber in self.subscribers.lock().unwrap().values().into_iter() {
 			match subscriber {
 				AnyObserver::ClojureObserver(clojure) => (clojure)(&t),
-				AnyObserver::Observer(ob) => (ob)(&t),
+				AnyObserver::Observer(ob) => ob.next(&t),
 			}
 		}
+		log::debug!("Notified all subscribers about {:?}", t);
 	}
 
-	fn subscribe(&mut self, observer: AnyObserver<T>) {
-		match observer {
-			AnyObserver::Observer(t) => self.subscribers.push(AnyObserver::Observer(t)),
-			AnyObserver::ClojureObserver(t) => {
-				self.subscribers.push(AnyObserver::ClojureObserver(t))
-			}
-		};
+	fn subscribe<'subscription>(
+		&'observable self,
+		observer: AnyObserver<T>,
+	) -> Subscription<'observable, T> {
+		let current_index: usize = *self.index.lock().unwrap();
+		log::debug!("Subscribe to index {}", current_index);
+
+		*self.index.lock().unwrap() += 1;
+
+		self.subscribers
+			.lock()
+			.unwrap()
+			.insert(current_index, observer);
+
+		Subscription::new(self, current_index)
 	}
 }
 
-pub trait Observer<T: Send + Debug>: Fn(&T) -> () {
-	fn receive(&self, value: &T) {
-		println!("{:?}", value);
+pub trait Observer<T: Send + Sized + Debug> {
+	fn next(&self, value: &T);
+}
+
+pub struct Subscription<'subscription, T: Send + Sized + Debug> {
+	observer: &'subscription Subject<'subscription, T>,
+	index: usize,
+	phantom: PhantomData<&'subscription i32>,
+}
+
+impl<'subscription, T: Send + Sized + Debug> Subscription<'subscription, T> {
+	fn new(
+		observer: &'subscription Subject<'subscription, T>,
+		index: usize,
+	) -> Subscription<'subscription, T> {
+		Subscription {
+			observer,
+			index,
+			phantom: PhantomData {},
+		}
+	}
+
+	pub fn unsubscribe(self) {
+		self.observer.unsubscribe(self.index)
 	}
 }
--- crates/dispatcher/src/main.rs
+++ crates/dispatcher/src/main.rs
@@ -1,16 +1,49 @@
-use dispatcher::{hello_dispatcher, Observable, Subject};
+use dispatcher::{Observable, Observer, Subject};
+
+struct SimpleObserver<T: std::fmt::Debug> {
+	id: T,
+}
+
+impl<T: std::fmt::Debug> SimpleObserver<T> {
+	fn new(id: T) -> Self {
+		Self { id }
+	}
+}
+
+impl<T: Send + Sync + std::fmt::Debug> Observer<T> for SimpleObserver<T> {
+	fn next(&self, value: &T) {
+		println!("SimpleObserver {:?} got value: {:?}", self.id, value);
+	}
+}
 
 pub fn main() {
-	hello_dispatcher();
-	let mut subject = Subject::<String>::new();
-	let observer = |message: &String| println!("Got Message! {:?}", message);
+	std::env::set_var("RUST_LOG", "debug");
+	env_logger::init();
+	log::debug!("Main started, env logger initialized");
 
-	subject.subscribe(dispatcher::AnyObserver::ClojureObserver(Box::new(observer)));
-	subject.subscribe(dispatcher::AnyObserver::ClojureObserver(Box::new(observer)));
+	let subject = Subject::<String>::new();
+	let simple_observer = SimpleObserver::new("s1".to_string());
 
-	subject.subscribe(dispatcher::AnyObserver::ClojureObserver(Box::new(observer)));
+	subject.next("No one will hear me!".to_string());
+
+	let simple_subscription =
+		subject.subscribe(dispatcher::AnyObserver::Observer(Box::new(simple_observer)));
+
+	subject.subscribe(dispatcher::AnyObserver::ClojureObserver(Box::new(
+		|message: &String| println!("Got Message in first subscription! {:?}", message),
+	)));
+	subject.subscribe(dispatcher::AnyObserver::ClojureObserver(Box::new(
+		|message: &String| println!("Got Message in second subscription! {:?}", message),
+	)));
+
+	let welcoming_subscription = subject.subscribe(dispatcher::AnyObserver::ClojureObserver(
+		Box::new(|message: &String| println!("Got Message in third subscription! {:?}", message)),
+	));
 
 	subject.next("Hello!".to_string());
+	simple_subscription.unsubscribe();
+
 	subject.next("And".to_string());
+	welcoming_subscription.unsubscribe();
 	subject.next("Welcome!".to_string());
 }
